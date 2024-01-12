0c964c39ecc95fefc2eb262b0fa0312c2d805cb3
carkhe <carloskiller177@gmail.com>
2024-01-12 09:22:19 -0600

    agregando seleccionar correo

--- leer_correo.py
+++ leer_correo.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import webbrowser
 import os
 from getpass import getpass
+import re
 
 load_dotenv()
 
@@ -21,7 +22,7 @@ imap.login(username, password)
 status, mensajes = imap.select("INBOX")
 # print(mensajes)
 # mensajes a recibir
-N = 1
+N = 10
 # cantidad total de correos
 mensajes = int(mensajes[0])
 
@@ -43,51 +44,55 @@ for i in range(mensajes, mensajes - N, -1):
                 subject = subject.decode()
             # de donde viene el correo
             from_ = mensaje.get("From")
-            print("Subject:", subject)
-            print("From:", from_)
-            print("Mensaje obtenido con exito")
-            # si el correo es html
-            if mensaje.is_multipart():
-                # Recorrer las partes del correo
-                for part in mensaje.walk():
-                    # Extraer el contenido
-                    content_type = part.get_content_type()
-                    content_disposition = str(part.get("Content-Disposition"))
-                    try:
-                        # el cuerpo del correo
-                        body = part.get_payload(decode=True).decode()
-                    except:
-                        pass
-                    if content_type == "text/plain" and "attachment" not in content_disposition:
-                        # Mostrar el cuerpo del correo
+            patron = r"<([a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})>"
+            correo_deseado = re.findall(patron,from_)
+            if correo_deseado[0] == "carloskiller177@gmail.com":
+                print("Subject:", subject)
+                print("From:", correo_deseado[0])
+                print("Mensaje obtenido con exito")
+                # si el correo es html
+                if mensaje.is_multipart():
+                    # Recorrer las partes del correo
+                    for part in mensaje.walk():
+                        # Extraer el contenido
+                        content_type = part.get_content_type()
+                        content_disposition = str(part.get("Content-Disposition"))
+                        try:
+                            # el cuerpo del correo
+                            body = part.get_payload(decode=True).decode()
+                        except:
+                            pass
+                        if content_type == "text/plain" and "attachment" not in content_disposition:
+                            # Mostrar el cuerpo del correo
+                            print(body)
+                        elif "attachment" in content_disposition:
+    #                         # download attachment
+                            nombre_archivo = part.get_filename()
+                            if nombre_archivo:
+                                if not os.path.isdir(subject):
+                                    # crear una carpeta para el mensaje
+                                    os.mkdir(subject)
+                                ruta_archivo = os.path.join(subject, nombre_archivo)
+                                # download attachment and save it
+                                open(ruta_archivo, "wb").write(part.get_payload(decode=True))
+                else:
+                    # contenido del mensaje
+                    content_type = mensaje.get_content_type()
+                    # cuerpo del mensaje
+                    body = mensaje.get_payload(decode=True).decode()
+                    if content_type == "text/plain":
+    #                     # mostrar solo el texto
                         print(body)
-                    elif "attachment" in content_disposition:
-#                         # download attachment
-                        nombre_archivo = part.get_filename()
-                        if nombre_archivo:
-                            if not os.path.isdir(subject):
-                                # crear una carpeta para el mensaje
-                                os.mkdir(subject)
-                            ruta_archivo = os.path.join(subject, nombre_archivo)
-                            # download attachment and save it
-                            open(ruta_archivo, "wb").write(part.get_payload(decode=True))
-            else:
-                # contenido del mensaje
-                content_type = mensaje.get_content_type()
-                # cuerpo del mensaje
-                body = mensaje.get_payload(decode=True).decode()
-                if content_type == "text/plain":
-#                     # mostrar solo el texto
-                    print(body)
-            # if content_type == "text/html":
-            #     # Abrir el html en el navegador
-            #     if not os.path.isdir(subject):
-            #         os.mkdir(subject)
-            #     nombre_archivo = f"{subject}.html"
-            #     ruta_archivo = os.path.join(subject, nombre_archivo)
-            #     open(ruta_archivo, "w").write(body)
-            #     # abrir el navegador
-            #     webbrowser.open(ruta_archivo)
-#             print("********************************")
+                # if content_type == "text/html":
+                #     # Abrir el html en el navegador
+                #     if not os.path.isdir(subject):
+                #         os.mkdir(subject)
+                #     nombre_archivo = f"{subject}.html"
+                #     ruta_archivo = os.path.join(subject, nombre_archivo)
+                #     open(ruta_archivo, "w").write(body)
+                #     # abrir el navegador
+                #     webbrowser.open(ruta_archivo)
+    #             print("********************************")
+                
 imap.close()
 imap.logout()
